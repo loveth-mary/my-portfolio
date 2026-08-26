a816d97b638d12d3ff84c6a8b1dd14dc1975e1ef
Loveth Mary <lovethmary.dev@gmail.com>
2026-08-26 18:09:13 +0100

    Add dark-mode to localStorage

--- script.js
+++ script.js
@@ -64,15 +64,30 @@ scrollElements.forEach((element) => {
 });
 
 
+
+// Toggle dark mode
 const themeToggle = document.querySelector(".theme-toggle");
+
+// Check saved theme when page loads
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    themeToggle.setAttribute("aria-label", "Switch to light mode");
+}
 
 themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+
         themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
         themeToggle.setAttribute("aria-label", "Switch to light mode");
     } else {
+        localStorage.setItem("theme", "light");
+
         themeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
         themeToggle.setAttribute("aria-label", "Switch to dark mode");
     }
